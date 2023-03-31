6ea8fcfc3badce9e078adbc58d3790ac81cf5c30
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-03-31 10:30:26 +0200

    Abstract Factory

--- src/main/java/com/ecommerce/thetasport/service/productabstractfactory/DirectorTennis.java
+++ src/main/java/com/ecommerce/thetasport/service/productabstractfactory/DirectorTennis.java
@@ -1,21 +1,20 @@
-package com.ecommerce.thetasport.service.productABF;
+package com.ecommerce.thetasport.service.productabstractfactory;
 
 import com.ecommerce.thetasport.model.ProductBean;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Classe che rappresenta il direttore per la creazione di prodotti del tipo "Tennis".
+ * Classe che rappresenta il direttore per la creazione di prodotti del tipo "Football".
  */
 public class DirectorTennis {
-
     /**
-     * Metodo statico che crea un prodotto del tipo "Tennis" sulla base delle informazioni contenute in un oggetto
+     * Metodo statico che crea un prodotto del tipo "Football" sulla base delle informazioni contenute in un oggetto
      * di tipo {@link ProductBean}.
      * @param productBean oggetto di tipo {@link ProductBean} che contiene le informazioni del prodotto da creare
      * @return un nuovo oggetto di tipo {@link Product} rappresentante il prodotto creato
      */
     public static Product createProduct(@NotNull ProductBean productBean) {
-        // Viene istanziata la factory specifica per i prodotti di tipo "Tennis"
+        // Viene istanziata la factory specifica per i prodotti di tipo "Football"
         TennisConcreteFactory tennisConcreteFactory = new TennisConcreteFactory();
         Product newProductTennis;
         // In base alla sottocategoria del prodotto da creare viene invocato il metodo corretto della factory
