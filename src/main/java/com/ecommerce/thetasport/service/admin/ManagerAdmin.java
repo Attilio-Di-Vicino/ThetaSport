6c1efd7cd153c65c7b1ff1cfeca965636c29d146
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-04-04 12:35:07 +0200

    TF-IDF

--- src/main/java/com/ecommerce/thetasport/service/admin/ManagerAdmin.java
+++ src/main/java/com/ecommerce/thetasport/service/admin/ManagerAdmin.java
@@ -4,7 +4,8 @@ import com.ecommerce.thetasport.dao.ProductDAO;
 import com.ecommerce.thetasport.model.ProductBean;
 import com.ecommerce.thetasport.service.tfidf.CustomPriorityQueue;
 import com.ecommerce.thetasport.service.tfidf.HelperTFIDF;
-import com.ecommerce.thetasport.service.tfidf.TFIDFCalculator;
+import com.ecommerce.thetasport.service.tfidf.ManagerTFIDF;
+import org.jetbrains.annotations.NotNull;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -13,16 +14,18 @@ import java.util.Map;
 
 public class ManagerAdmin {
 
-    public static List< ProductBean > getOfferSingleUser( String email, int numberOfProductForOffer ) throws SQLException {
+    private static final int NUMBER_RELATED_OFFERS = 3;
+
+    public static @NotNull List< ProductBean > getOfferSingleUser(String email ) throws SQLException {
         List< ProductBean > productBeanListOffer = new ArrayList<>();
-        CustomPriorityQueue< String, Double > resultTFIDF = TFIDFCalculator.TFIDFSingleUser( email, 10 );
+        CustomPriorityQueue< String, Double > resultTFIDF = ManagerTFIDF.TFIDFSingleUser( email );
         System.out.println( resultTFIDF );
         List< ProductBean > productBeanList = ProductDAO.getProductBeanList();
         // qui c'è da capire se è meglio eliminare il term dalla queue o no
-        while ( !resultTFIDF.isEmpty() && productBeanListOffer.size() < numberOfProductForOffer ) {
+        while ( !resultTFIDF.isEmpty() && productBeanListOffer.size() < NUMBER_RELATED_OFFERS ) {
             Map.Entry< String, Double > amap = resultTFIDF.poll();
             for ( ProductBean productBean : productBeanList ) {
-                if ( productBeanListOffer.size() >= numberOfProductForOffer ) {
+                if ( productBeanListOffer.size() >= NUMBER_RELATED_OFFERS ) {
                     break;
                 }
                 List< String > wordList = HelperTFIDF.convertStringToWordList( productBean.getName() );
@@ -30,7 +33,7 @@ public class ManagerAdmin {
                     if ( word.equals( amap.getKey() ) ) {
                         productBeanListOffer.add( productBean );
                     }
-                    if ( productBeanListOffer.size() >= numberOfProductForOffer ) {
+                    if ( productBeanListOffer.size() >= NUMBER_RELATED_OFFERS ) {
                         break;
                     }
                 }
@@ -40,7 +43,7 @@ public class ManagerAdmin {
     }
 
     public static void main( String[] args ) throws SQLException {
-        List< ProductBean > productBeanList = getOfferSingleUser( "attilio@gmail.com", 40 );
+        List< ProductBean > productBeanList = getOfferSingleUser( "attilio@gmail.com" );
         System.out.println( "\n*** TEST TF-IDF OFFER ***" );
         System.out.println( "Number of products for offer is: " + productBeanList.size() );
         System.out.println( productBeanList );
