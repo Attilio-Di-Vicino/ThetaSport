80af645a3df7a02a840baf438b94ef9e7488a0a8
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-04-07 00:30:00 +0200

    formatting

--- src/main/java/com/ecommerce/thetasport/service/loginCor/HelperCoR.java
+++ src/main/java/com/ecommerce/thetasport/service/loginCor/HelperCoR.java
@@ -1,7 +1,6 @@
 package com.ecommerce.thetasport.service.loginCor;
 
 import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * La classe HelperCoR contiene metodi utili per la gestione della catena di responsabilità.<br>
@@ -20,12 +19,12 @@ public class HelperCoR {
      * Il secondo _ indica che il metodo accetta un qualsiasi tipo di parametro come secondo parametro.<br>
      * In input assume un numero indefinito di handler
      *
-     * @param head     Il primo handler della catena
+     * @param head Il primo handler della catena
      * @param handlers Gli handler successivi da collegare
      * @return L'handler iniziale della catena di responsabilità
      */
     @Contract("_, _ -> param1")
-    public static Handler linkHandler( Handler head, Handler @NotNull ...handlers ) {
+    public static Handler linkHandler( Handler head, Handler ...handlers ) {
         Handler first = head;
         for ( Handler nextInChain : handlers ) {
             first.setNextHandler( nextInChain );
