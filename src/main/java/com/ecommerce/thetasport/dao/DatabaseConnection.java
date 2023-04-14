a74e1adfe147126b0ce7d9502747cdb7c9856853
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-04-14 17:02:29 +0200

    comment

--- src/main/java/com/ecommerce/thetasport/dao/DatabaseConnection.java
+++ src/main/java/com/ecommerce/thetasport/dao/DatabaseConnection.java
@@ -4,10 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * La classe DatabaseConnection è responsabile della connessione al database. <br>
+ * Utilizza il pattern Singleton per garantire che venga creata una sola <br>
+ * istanza della connessione al database.
+ *
+ * @author Theta Sport
+ * @version 1.0
+ */
 public class DatabaseConnection {
     private static DatabaseConnection instance;
     private Connection connection;
 
+    /**
+     * Costruttore privato della classe DatabaseConnection. <br>
+     * Viene invocato una sola volta per creare una nuova connessione al database. <br>
+     * Se la connessione fallisce, viene lanciata un'eccezione di tipo SQLException.
+     *
+     * @throws SQLException in caso di errori durante la creazione della connessione
+     */
     private DatabaseConnection() throws SQLException {
         try {
             Class.forName( "com.mysql.cj.jdbc.Driver" );
@@ -18,10 +33,22 @@ public class DatabaseConnection {
         }
     }
 
+    /**
+     * Metodo pubblico che restituisce la connessione al database.
+     *
+     * @return la connessione al database
+     */
     public Connection getConnection() {
         return connection;
     }
 
+    /**
+     * Metodo pubblico che restituisce l'unica istanza della classe DatabaseConnection. <br>
+     * Se l'istanza non è ancora stata creata, viene creata e restituita.
+     *
+     * @return l'unica istanza della classe DatabaseConnection
+     * @throws SQLException in caso di errori durante la creazione della connessione
+     */
     public static DatabaseConnection getInstance() throws SQLException {
         if ( instance == null ) {
             instance = new DatabaseConnection();
--- src/main/java/com/ecommerce/thetasport/dao/HelperDAO.java
+++ src/main/java/com/ecommerce/thetasport/dao/HelperDAO.java
@@ -11,12 +11,21 @@ import java.sql.SQLException;
 import java.util.Calendar;
 
 /**
- * La classe HelperDAO contiene metodi utili per la gestione dei DAO.
+ * La classe HelperDAO contiene metodi utili per la gestione dei DAO. <br>
+ * Questi metodi vengono utilizzati per ottenere informazioni sui prodotti e gestire il database.
  *
  * @author Theta Sport
  * @version 1.0
  */
 public class HelperDAO {
+
+    /**
+     * Imposta i valori di ProductBean con i dati restituiti da ResultSet.
+     *
+     * @param productBean il bean del prodotto da aggiornare
+     * @param rs il ResultSet contenente i dati del prodotto
+     * @throws SQLException se si verifica un errore durante l'accesso al database
+     */
     public static void setSingleProductBean( @NotNull ProductBean productBean, @NotNull ResultSet rs ) throws SQLException {
         productBean.setCode( rs.getInt( "CODE" ) );
         productBean.setName( rs.getString( "NAME" ) );
@@ -28,6 +37,13 @@ public class HelperDAO {
         productBean.setImage( rs.getString( "IMAGE" ) );
     }
 
+    /**
+     * Modifica il valore di una colonna nella tabella dei prodotti nel database.
+     *
+     * @param update il nuovo valore da inserire
+     * @param code il codice del prodotto da modificare
+     * @param query la query SQL per eseguire l'aggiornamento
+     */
     public static void editProduct( String update, int code, String query) {
         try ( PreparedStatement pstmt = DatabaseConnection.getInstance().getConnection().prepareStatement( query ) ) {
             pstmt.setString(1, update);
@@ -38,11 +54,21 @@ public class HelperDAO {
         }
     }
 
+    /**
+     * Restituisce il numero del mese corrente (1-12).
+     *
+     * @return il numero del mese corrente
+     */
     public static int getCurrentMonth() {
         Calendar cal = Calendar.getInstance();
         return cal.get( Calendar.MONTH ) + 1; // month index starts from 0, so 1 is added
     }
 
+    /**
+     * Restituisce l'anno corrente.
+     *
+     * @return l'anno corrente
+     */
     public static int getCurrentYear() {
         Calendar cal = Calendar.getInstance();
         return cal.get( Calendar.YEAR );
